0130b8df2d2aeb04cb69c6b867778911532ddd09
Akbar <babaevakbars@gmail.com>
2025-01-02 20:42:56 +0500

    first commit

---
+++ Lesson#3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson3
+{
+    class Lesson3
+    {
+        public void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello");
+        }
+
+    }
+}
