c9106cbadd2e714eeac7805a027d9a5ba44df5c2
Akbar <babaevakbars@gmail.com>
2025-01-02 20:54:04 +0500

    Задание1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson3
+{
+    class Lesson3
+    {
+        static void Main(string[] args)
+        {
+            //Задание1
+            System.Console.WriteLine("Введите первое число:");
+            int a = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Введите второе число:");
+            int b = int.Parse(Console.ReadLine());
+
+            if(a > b)
+            {
+                b = a;
+            }
+            else if(a < b)
+            {
+                a = b;
+            }
+            else if(a == b)
+            {
+                a = 0;
+                b = 0;
+            }
+            System.Console.WriteLine($"a = {a}");
+            System.Console.WriteLine($"b = {b}");
+        }
+
+    }
+}
